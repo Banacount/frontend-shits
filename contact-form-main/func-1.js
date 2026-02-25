a87b40b9c31a110a795b938bb5a0bf7b9120fe7b
Banacount <uniquejmal@gmail.com>
2026-02-25 18:25:40 +0800

    Added some animasyon and message sent thingy

--- contact-form-main/func-1.js
+++ contact-form-main/func-1.js
@@ -1,11 +1,13 @@
 
 const generalEnq = document.getElementById("generalEnquiryCon");
 const supportRequest = document.getElementById("supportRequestCon");
-const consentBox = document.getElementById("consentBoxContain")
+const consentBox = document.getElementById("consentBoxContain");
+const formSucBox = document.getElementById("formSuccess");
 
 const generalEnqRadio = document.getElementById("generalEnquiry");
 const supportRequestRadio = document.getElementById("supportRequest");
 const consentBoxRadio = document.getElementById("consentCheckbox");
+const contactForm = document.getElementById("contactForm");
 
 let general_enquiry_switch = false;
 let support_request_switch = false;
@@ -29,3 +31,14 @@ consentBox.addEventListener('click', () => {
     consent_box_switch = !consent_box_switch;
     consentBoxRadio.checked = consent_box_switch;
 });
+
+const urlParams = new URLSearchParams(window.location.search);
+if (urlParams.has('consent-checkbox')) {
+    setTimeout(() => {
+        formSucBox.style.transform = "translateY(0)";
+    }, 500);
+
+    setTimeout(() => {
+        formSucBox.style.transform = "translateY(calc(-100% - 2rem))";
+    }, 8000);
+}
